d06f79907c9cefb2f1d45d140d893ad45f4c7205
agathemaugendre <agathe.maugendre@edu.esiee.fr>
2024-11-14 10:32:41 +0000

    05-palindromes Agathe Maugendre G7

--- main.py
+++ main.py
@@ -1,18 +1,49 @@
-#### Fonction secondaire
-
+"""
+Module pour vérifier si une chaîne de caractères est un palindrome.
+La fonction ispalindrome() ignore les majuscules, les accents et les caractères spéciaux 
+pour effectuer la vérification du palindrome.
+"""
+import re
+import unidecode
 
 def ispalindrome(p):
 
-    # votre code ici
+    """ 
+    Vérifie si la chaîne de caractère passé en paramètre est un palindrome.
     
-    return False
+    La fonction ignore tout ce qui est accent et ponctuation, pour cela on utilise
+    des méthodes importés pour faciliter la conversion.
+    
+    Exemple d'utilisation :
+    
+    >>> ispalindrome("trousse")
+    False
+    >>> ispalindrome("ressasser")
+    True
+    >>> ispalindrome("été")
+    True
+    >>> ispalindrome("table")
+    False
+
+    """
+    p = p.lower()
+    p = unidecode.unidecode(p)  #Converti les caractères spéciaux
+    p = re.sub(r'[^a-zA-Z0-9]', '', p) #Supprimer les caractères non "alphanumériques"
+
+    return p == p[::-1]
 
 #### Fonction principale
 
-
 def main():
 
-    # vos appels à la fonction secondaire ici
+    """ 
+    Fonction qui teste le fonctionnement de ispalindrome()
+    """
+    # Appel de la fonction ispalindrome pour différents exemples
+    ispalindrome("trousse")
+    ispalindrome("ressasser")
+    ispalindrome("été")
+    ispalindrome("table")
 
     for s in ["radar", "kayak", "level", "rotor", "civique", "deifie"]:
         print(s, ispalindrome(s))
